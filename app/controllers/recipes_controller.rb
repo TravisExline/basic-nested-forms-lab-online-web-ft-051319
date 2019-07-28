d49570089dd48ced0307dd31b8966c0268ffc3ac
Travis Exline <t.exline22@gmail.com>
2019-07-28 14:46:29 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -25,7 +25,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).premit(:title,
+    params.require(:recipe).permit(:title,
     ingredient_attributes: [
       :name,
       :quantity
